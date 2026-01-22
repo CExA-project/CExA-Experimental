615cc7729dda8a95fe390885c40a5985f16a66e6
Paul Gannay <paul.gannay@cea.fr>
2026-01-22 16:39:45 +0100

    no inline

--- variants/tests/bench.cpp
+++ variants/tests/bench.cpp
@@ -10,13 +10,13 @@
 // loop using Kokkos
 namespace virt {
 struct A {
-  virtual int f(int in) { return 4 + in; }
+  __attribute__((noinline))virtual int f(int in) { return 4 + in; }
 };
 struct B : A {
-  int f(int in) { return 6 + in; }
+__attribute__((noinline))  int f(int in) { return 6 + in; }
 };
 struct C : A {
-  int f(int in) { return 2 * A::f(in); }
+__attribute__((noinline))  int f(int in) { return 2 * A::f(in); }
 };
 
 }  // namespace virt
@@ -24,31 +24,43 @@ struct C : A {
 
 static void Virt(benchmark::State& state) {
   const int size = 1000002;
-  std::vector<virt::A*> v(size);
-  for (int i = 0; i < size; i+=3) {
-    v[0+i] = new virt::A;
-    v[1+i] = new virt::B;
-    v[2+i] = new virt::C;
-  }
+  //std::vector<virt::A*> v(size);
+  //for (int i = 0; i < size; i+=3) {
+  //  v[0+i] = new virt::A;
+  //  v[1+i] = new virt::B;
+  //  v[2+i] = new virt::C;
+  //}
+  std::vector<virt::A*> v(3);
+  //for (int i = 0; i < size; i+=3) {
+    v[0] = new virt::A;
+    v[1] = new virt::B;
+    v[2] = new virt::C;
+  //}
 
   std::vector<int> res(size);
 
   for (auto _ : state) {
     for (int i=0; i<size; ++i) {
-      res[i] = v[i]->f(i);
+      res[i] = v[i%3]->f(i);
     }
+    benchmark::DoNotOptimize(res);
+    //benchmark::ClobberMemory();
+  }
+
+  for (int i = 0; i < 3; i+=3) {
+    delete v[i];
   }
 }
 
 namespace virtDevice {
 struct A {
-  KOKKOS_FUNCTION virtual int f(int in) { return 4 + in; }
+__attribute__((noinline))  KOKKOS_FUNCTION virtual int f(int in) { return 4 + in; }
 };
 struct B : A {
-  KOKKOS_FUNCTION int f(int in) { return 6 + in; }
+__attribute__((noinline))  KOKKOS_FUNCTION int f(int in) { return 6 + in; }
 };
 struct C : A {
-  KOKKOS_FUNCTION int f(int in) { return 2 * A::f(in); }
+__attribute__((noinline))  KOKKOS_FUNCTION int f(int in) { return 2 * A::f(in); }
 };
 
 }  // namespace virt
@@ -74,13 +86,13 @@ static void VirtDevice(benchmark::State& state) {
       // initialize on device
       switch (i%3) {
         case 0: 
-          new (static_cast<virtDevice::A*>(deviceInstanceMemory + virt_idx)) virtDevice::A();
+          new (static_cast<virtDevice::A*>((void*)((uint8_t*)(deviceInstanceMemory) + virt_idx))) virtDevice::A();
           break;
         case 1: 
-          new (static_cast<virtDevice::B*>(deviceInstanceMemory + virt_idx)) virtDevice::B();
+          new (static_cast<virtDevice::B*>((void*)((uint8_t*)(deviceInstanceMemory) + virt_idx))) virtDevice::B();
           break;
         case 2: 
-          new (static_cast<virtDevice::C*>(deviceInstanceMemory + virt_idx)) virtDevice::C();
+          new (static_cast<virtDevice::C*>((void*)((uint8_t*)(deviceInstanceMemory) + virt_idx))) virtDevice::C();
           break;
       }
 
@@ -93,7 +105,7 @@ static void VirtDevice(benchmark::State& state) {
         Kokkos::RangePolicy<Kokkos::DefaultExecutionSpace>(0, size),
         KOKKOS_LAMBDA(int i) {
           size_t virt_idx = deriv_size * i;
-          virtDevice::A* a = static_cast<virtDevice::A*>(deviceInstanceMemory + virt_idx);
+          virtDevice::A* a = static_cast<virtDevice::A*>((void*)((uint8_t*)(deviceInstanceMemory) + virt_idx));
           res[i] = a->f(i);
         });
     Kokkos::fence();
@@ -104,33 +116,37 @@ static void VirtDevice(benchmark::State& state) {
 }
 
 namespace FunctionPtr {
-  int fA(int in) { return 4 + in; }
-  int fB(int in) { return 6 + in; }
-  int fC(int in) { return 2 * fA(in); }
+__attribute__((noinline))  int fA(int in) { return 4 + in; }
+__attribute__((noinline))  int fB(int in) { return 6 + in; }
+__attribute__((noinline))  int fC(int in) { return 2 * fA(in); }
 }  // namespace FunctionPtr
 
 static void FctPtr(benchmark::State& state) {
   const int size = 1000002;
-  std::vector<int (*)(int)> v(size);
-  for (int i = 0; i < size; i+=3) {
-    v[i + 0] = &FunctionPtr::fA;
-    v[i + 1] = &FunctionPtr::fB;
-    v[i + 2] = &FunctionPtr::fC;
-  }
+  //std::vector<int (*)(int)> v(size);
+  std::vector<int (*)(int)> v(3);
+
+  //for (int i = 0; i < size; i+=3) {
+    v[0] = &FunctionPtr::fA;
+    v[1] = &FunctionPtr::fB;
+    v[2] = &FunctionPtr::fC;
+  //}
 
   std::vector<int> res(size);
 
   for (auto _ : state) {
     for (int i=0; i<size; ++i) {
-      res[i] = v[i](i);
+      res[i] = v[i%3](i);
     }
+    benchmark::DoNotOptimize(res);
+    benchmark::ClobberMemory();
   }
 }
 
 namespace FunctionPtrDevice {
-  KOKKOS_FUNCTION int fA(int in) { return 4 + in; }
-  KOKKOS_FUNCTION int fB(int in) { return 6 + in; }
-  KOKKOS_FUNCTION int fC(int in) { return 2 * fA(in); }
+__attribute__((noinline))  KOKKOS_FUNCTION int fA(int in) { return 4 + in; }
+__attribute__((noinline))  KOKKOS_FUNCTION int fB(int in) { return 6 + in; }
+__attribute__((noinline))  KOKKOS_FUNCTION int fC(int in) { return 2 * fA(in); }
 }  // namespace FunctionPtr
 
 static void FctPtrDevice(benchmark::State& state) {
@@ -161,13 +177,13 @@ static void FctPtrDevice(benchmark::State& state) {
 namespace Kokkos_port {
 struct A {
   // `virtual` keyword has to be removed for portability with SYCL
-  KOKKOS_FUNCTION /*virtual*/ int f(int in) { return 4 + in; }
+__attribute__((noinline))  KOKKOS_FUNCTION /*virtual*/ int f(int in) { return 4 + in; }
 };
 struct B : A {
-  KOKKOS_FUNCTION int f(int in) { return 6 + in; }
+__attribute__((noinline))  KOKKOS_FUNCTION int f(int in) { return 6 + in; }
 };
 struct C : A {
-  KOKKOS_FUNCTION int f(int in) { return 2 * A::f(in); }
+__attribute__((noinline))  KOKKOS_FUNCTION int f(int in) { return 2 * A::f(in); }
 };
 
 using A_Hierarchy = cexa::experimental::variant<A, B, C>;
@@ -205,9 +221,9 @@ static void Variants(benchmark::State& state) {
   }
 }
 
-KOKKOS_FUNCTION int fA(int in) { return 4 + in; }
-KOKKOS_FUNCTION int fB(int in) { return 6 + in; }
-KOKKOS_FUNCTION int fC(int in) { return 2 * fA(in); }
+__attribute__((noinline)) KOKKOS_FUNCTION int fA(int in) { return 4 + in; }
+__attribute__((noinline)) KOKKOS_FUNCTION int fB(int in) { return 6 + in; }
+__attribute__((noinline)) KOKKOS_FUNCTION int fC(int in) { return 2 * fA(in); }
 
 template <class MemorySpace>
 static void Switch(benchmark::State& state) {
@@ -236,46 +252,47 @@ static void Switch(benchmark::State& state) {
   }
 }
 
-static void Switch(benchmark::State& state) {
+static void SwitchNoKokkos(benchmark::State& state) {
   const int size = 1000002;
 
   std::vector<int> res(size);
 
   for (auto _ : state) {
-    for (int i=0; i<size; ++i) {
-          switch (i%3) {
-            case 0: 
-              res[i] = fA(i);
-              break;
-            case 1: 
-              res[i] = fB(i);
-              break;
-            case 2: 
-              res[i] = fC(i);
-              break;
-          }
+    for (int i=0; i < size; ++i) {
+        switch (i%3) {
+          case 0: 
+            res[i] = fA(i);
+            break;
+          case 1: 
+            res[i] = fB(i);
+            break;
+          case 2: 
+            res[i] = fC(i);
+            break;
+        }
     }
+    benchmark::DoNotOptimize(res);
+    //benchmark::ClobberMemory();
   }
 }
 
 static void VariantsNoKokkos(benchmark::State& state) {
   const int size = 1000002;
-  Kokkos::View<Kokkos_port::A_Hierarchy*, Kokkos::DefaultHostExecutionSpace> v("view", size);
-  auto v_h = Kokkos::create_mirror_view(v);
+  std::vector<Kokkos_port::A_Hierarchy> v(size);
   for (int i = 0; i < size; i+=3) {
-    v_h(i + 0) = Kokkos_port::A{};
-    v_h(i + 1) = Kokkos_port::B{};
-    v_h(i + 2) = Kokkos_port::C{};
+    v[i + 0] = Kokkos_port::A{};
+    v[i + 1] = Kokkos_port::B{};
+    v[i + 2] = Kokkos_port::C{};
   }
 
-  Kokkos::deep_copy(v, v_h);
-
-  Kokkos::View<int*, Kokkos::DefaultHostExecutionSpace> res("res", size);
+  std::vector<int> res(size);
 
   for (auto _ : state) {
     for (int i = 0; i<size; ++i) {
-          res(i) = Kokkos_port::call_f(v(i), i);
+      res[i] = Kokkos_port::call_f(v[i], i);
     }
+    benchmark::DoNotOptimize(res);
+    benchmark::ClobberMemory();
   }
 }
 
@@ -306,19 +323,22 @@ static void stdvariant(benchmark::State& state) {
     for (int i = 0; i<size; ++i) {
           res(i) = call_f(v(i), i);
     }
+    benchmark::DoNotOptimize(res);
+    benchmark::ClobberMemory();
   }
 }
 
 int main(int argc, char** argv) {
   Kokkos::ScopeGuard guard(argc, argv);
 
-  //benchmark::MaybeReenterWithoutASLR(argc, argv);
+  srand(0);
+
   benchmark::RegisterBenchmark("virt", Virt);
   benchmark::RegisterBenchmark("fctPtr", FctPtr);
   benchmark::RegisterBenchmark("variants_host", Variants<Kokkos::DefaultHostExecutionSpace>);
   benchmark::RegisterBenchmark("variantsNoKokkos", VariantsNoKokkos);
   benchmark::RegisterBenchmark("switch_host", Switch<Kokkos::DefaultHostExecutionSpace>);
-  benchmark::RegisterBenchmark("switch_hostnoKokkos", Switch);
+  benchmark::RegisterBenchmark("switch_hostnoKokkos", SwitchNoKokkos);
   benchmark::RegisterBenchmark("stdvariant(host)", stdvariant);
   benchmark::RegisterBenchmark("VirtDevice", VirtDevice);
   benchmark::RegisterBenchmark("fctPtrDevice", FctPtrDevice);
