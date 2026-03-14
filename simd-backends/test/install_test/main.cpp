a3476a485860f177bd58374165b6b0b9d2112e41
Trévis Morvany <trevismorvany@gmail.com>
2026-03-14 17:35:00 +0100

    Overhaul the CMake files - don't install gtest when tests are enabled - Bump cmake min version to 3.23 - Use file sets - Use configure_file to pass the macros for selecting sleef or svml

---
+++ simd-backends/test/install_test/main.cpp
@@ -0,0 +1,10 @@
+#include <Kokkos_Core.hpp>
+#include <Kokkos_SIMD.hpp>
+#include <CEXA_SIMD_Backends.hpp>
+
+int main(int argc, char* argv[]) {
+  Kokkos::ScopeGuard kokkos_scope(argc, argv);
+
+  Kokkos::Experimental::simd<float> vec(1.f);
+  vec = Kokkos::exp(vec);
+}
