3d3db37b8fc14c2ddd4e2a6c83385917a1980bd3
Adrien Taberner <adrien.taberner@cea.fr>
2026-05-05 14:29:22 +0200

    Fix Tests
    Signed-off-by: Adrien Taberner <adrien.taberner@cea.fr>

--- archInfo/test/unit_test/TestArchInfo.cpp
+++ archInfo/test/unit_test/TestArchInfo.cpp
@@ -8,49 +8,39 @@
 
 // OS
 TEST(ArchInfo, KernelVersion) {
-  ASSERT_GT(cexa::impl::get_kernel_version().size(), 0);
+  ASSERT_GT(cexa::get_kernel_version().size(), 0);
 }
 
-TEST(ArchInfo, SysName) {
-  ASSERT_GT(cexa::impl::get_sys_name().size(), 0);
-}
+TEST(ArchInfo, SysName) { ASSERT_GT(cexa::get_sys_name().size(), 0); }
 
-TEST(ArchInfo, SysType) {
-  ASSERT_GT(cexa::impl::get_sys_type().size(), 0);
-}
+TEST(ArchInfo, SysType) { ASSERT_GT(cexa::get_sys_type().size(), 0); }
 
 // CPU
 TEST(ArchInfo, CPUModelName) {
-  ASSERT_GT(cexa::impl::get_cpu_model_name().size(), 0);
+  ASSERT_GT(cexa::get_cpu_model_name().size(), 0);
 }
 
-TEST(ArchInfo, SocketCount) {
-  ASSERT_GT(cexa::impl::get_physical_socket_count(), 0);
-}
+TEST(ArchInfo, SocketCount) { ASSERT_GT(cexa::get_physical_socket_count(), 0); }
 
 TEST(ArchInfo, CoreCountPerSocket) {
-  ASSERT_GT(cexa::impl::get_core_count_per_socket(), 0);
+  ASSERT_GT(cexa::get_core_count_per_socket(), 0);
 }
 
 TEST(ArchInfo, ThreadCountPerSocket) {
-  ASSERT_GT(cexa::impl::get_thread_count_per_socket(), 0);
+  ASSERT_GT(cexa::get_thread_count_per_socket(), 0);
 }
 
 // GPU
-TEST(ArchInfo, GPUName) {
-  ASSERT_GT(cexa::impl::get_gpu_name().size(), 0);
-}
+TEST(ArchInfo, GPUName) { ASSERT_GT(cexa::get_gpu_name().size(), 0); }
 
-TEST(ArchInfo, GPUArch) {
-  ASSERT_GT(cexa::impl::get_gpu_arch().size(), 0);
-}
+TEST(ArchInfo, GPUArch) { ASSERT_GT(cexa::get_gpu_arch().size(), 0); }
 
 TEST(ArchInfo, GPUDriverVersion) {
-  ASSERT_GT(cexa::impl::get_gpu_driver_version().size(), 0);
+  ASSERT_GT(cexa::get_gpu_driver_version().size(), 0);
 }
 
 TEST(ArchInfo, GPURuntimeVersion) {
-  ASSERT_GT(cexa::impl::get_gpu_runtime_version().size(), 0);
+  ASSERT_GT(cexa::get_gpu_runtime_version().size(), 0);
 }
 
 int main(int argc, char *argv[]) {
@@ -60,4 +50,3 @@ int main(int argc, char *argv[]) {
   Kokkos::finalize();
   return result;
 }
-
